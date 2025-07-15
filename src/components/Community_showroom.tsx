33947ddddc37fe6581275b8c3900f9540adadc29
hayeonggim1 <hayeonggim1@gmail.com>
2025-07-15 09:51:07 +0900

    💄 style: 커뮤니티 사진 추가 및 메인페이지 커뮤니티 컴포넌트 스타일 수정

--- src/components/Community_showroom.tsx
+++ src/components/Community_showroom.tsx
@@ -7,12 +7,12 @@ interface HousePost{
 }
 
 const dummyHousePosts: HousePost[] = [
-  { id: 1, title: '제목', imgUrl: '/' },
-  { id: 2, title: '제목', imgUrl: '/' },
-  { id: 3, title: '제목', imgUrl: '/' },
-  { id: 4, title: '제목', imgUrl: '/' },
-  { id: 5, title: '제목', imgUrl: '/' },
-  { id: 6, title: '제목', imgUrl: '/' },
+  { id: 1, title: '유럽 에어비엔비처럼 꾸민 자취방', imgUrl: '/image/europeShowroom.png' },
+  { id: 2, title: '제주 게스트하우스 느낌으로 꾸민 룸', imgUrl: '/image/jejuShowroom.png' },
+  { id: 3, title: '18평 빌라에서 만든 초록색 신혼집', imgUrl: '/image/greenShowroom.png' },
+  { id: 4, title: '식물과 빈티지 소품으로 꾸민 8층 복층', imgUrl: '/image/whiteShowroom.png' },
+  { id: 5, title: '유럽 에어비엔비처럼 꾸민 자취방', imgUrl: '/image/europeShowroom.png' },
+  { id: 6, title: '제주 게스트하우스 느낌으로 꾸민 룸', imgUrl: '/image/jejuShowroom.png' },
 ];
 
 function ShowRoom(){
@@ -21,7 +21,7 @@ function ShowRoom(){
     <div className="community-wrapper w-3xl outline-2">
         <div className="p-10" >
           <Title title={'집들이🏠'} subTitle={'우리집에 왜 왔니'}></Title>
-            <div className='grid grid-cols-3 gap-10'>
+            <div className='grid grid-cols-3 gap-5'>
               {dummyHousePosts.map((post) => (
               <div
                 key={post.id}
@@ -32,7 +32,7 @@ function ShowRoom(){
                   alt='썸네일'
                   className="w-full h-30 object-cover mb-3 bg-livealone-columbia-blue"
                 />
-                <p className="text-sm text-gray-800">{post.title}</p>
+                <p className="text-xs font-variable font-bold text-gray-800">{post.title}</p>
               </div>
             ))}
             </div>
