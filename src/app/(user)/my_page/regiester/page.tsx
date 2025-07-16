b2e3fd5591f73f53bb68a3689dad410642837f12
J.H.S_0616 <johyunsoo369@naver.com>
2025-07-16 09:30:07 +0900

    🎨 feat: 로그인/회원가입 컴포넌트 구현 💄 style: 버튼 디자인 3d형식으로 수정 ✅ add: 프로필 기본 이미지 추가 ♻️ refactor:  페이지 링크 연결 및 inputID 컴포넌트 필요에 맞게 수정

---
+++ src/app/(user)/my_page/regiester/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+import InputId from '@/components/Input/Input_id';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Register() {
+  const [imageSrc, setImageSrc] = useState('');
+
+  const handleFilePath = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('파일 선택 이벤트 발생!');
+    const target = e.target as HTMLInputElement;
+    const file = target.files?.[0];
+    if (file) {
+      setImageSrc(URL.createObjectURL(file));
+    }
+  };
+
+  const nextPage = use
+  return (
+    <main className="bg-white pb-40">
+      <h3 className="font-logo text-5xl text-button-color pt-24 text-center">
+        REGISTER
+      </h3>
+      <div className="border-2 mt-8 m-auto w-[46.25rem] rounded-4xl border-button-color-opaque-25 px-20 py-24">
+        <form action="">
+          <div className="flex items-center gap-10">
+            <Image
+              src={imageSrc || '/image/profile.png'}
+              alt="프로필 사진"
+              width={112}
+              height={112}
+              className="rounded-[100%] border-2 border-black w-28 h-28 mask-radial-at-center object-cover bg-gray-200"
+            />
+            <label
+              htmlFor="user_profile"
+              className="nahonsan-btn-3d-vanilla rounded-full font-basic font-bold bg-vanilla-300  p-2.5 cursor-pointer"
+            >
+              사진 추가하기
+            </label>
+            <input
+              type="file"
+              name="user_profile"
+              id="user_profile"
+              className="hidden"
+              accept="image/*"
+              onChange={handleFilePath}
+            />
+          </div>
+          <InputId
+            text={'아이디'}
+            placeholder={'사용이 가능한 이메일을 입력해주세요'}
+            idValue={'user_id'}
+            inputType={'text'}
+          />
+          <InputId
+            text={'닉네임'}
+            placeholder={'사용하실 닉네임을 입력해주세요'}
+            idValue={'user_nickname'}
+            inputType={'text'}
+          />
+          <InputId
+            text={'이름'}
+            placeholder={'성함을 입력해주세요'}
+            idValue={'user_name'}
+            inputType={'text'}
+          />
+          <InputId
+            text={'비밀번호'}
+            placeholder={'비밀번호를 입력해주세요'}
+            idValue={'userPW'}
+            inputType={'password'}
+          />
+          <InputId
+            text={'휴대전화 번호'}
+            placeholder={'핸드폰 번호를 -를 제외하고 입력해주세요'}
+            idValue={'user_phone_number'}
+            inputType={'text'}
+          />
+          <InputId
+            text={'배송지'}
+            placeholder={'상품을 수령할 주소를 입력해주세요'}
+            idValue={'user_address'}
+            inputType={'text'}
+          />
+          <button
+            type="submit"
+            className="block nahonsan-btn-3d-sky border-button-color mx-auto rounded-radius-full mt-16 py-4 px-8 font-basic tracking-paragraph-default font-bold text-size-md"
+            onClick={}
+          >
+            다음페이지
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
