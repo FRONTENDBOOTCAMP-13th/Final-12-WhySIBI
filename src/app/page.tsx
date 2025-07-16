ccab2885d6d170208feb0f024906294ff8c875dc
Yujiin <yjjeong0528@gmail.com>
2025-07-16 09:27:01 +0900

    🎨  feat: 메인페이지 배너 슬라이더 구현 #56

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import MainBannerSlider from '@/components/main_banner_slider';
 import MainProductSlider from '@/components/main_product_slider';
 
 export default function Home() {
   return (
     <main>
+      <h1>배너</h1>
+      <MainBannerSlider />
       <h1>나혼산 BEST 상품</h1>
       <MainProductSlider />
     </main>
