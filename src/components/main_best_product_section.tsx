2bc76c4336488cf075497f2c0abd27da4131d61e
Yujiin <yjjeong0528@gmail.com>
2025-07-17 10:54:12 +0900

    🎨 feat:  타임스탬프 기능 추가

--- src/components/main_best_product_section.tsx
+++ src/components/main_best_product_section.tsx
@@ -1,4 +1,22 @@
+'use client';
+
+import MainProductSlider from '@/components/main_product_slider';
+import TimeStamp from '@/components/Time_stamp';
+import Title from '@/components/Title';
+
 function MainBestProductSection() {
-  return <></>;
+  return (
+    <>
+      <Title
+        title="나혼산 BEST 상품"
+        subTitle={
+          <>
+            현재시각 <TimeStamp /> 기준 인기상품 🔥🔥🔥
+          </>
+        }
+      />
+      <MainProductSlider />
+    </>
+  );
 }
 export default MainBestProductSection;
