a3d9dd3291584258b82eca1949ea2101c8f6e82c
Yujiin <yjjeong0528@gmail.com>
2025-07-17 15:47:20 +0900

    🎨 feat:  메인 하단 슬라이더 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import MainBannerSlider from '@/components/main_banner_slider';
-import MainProductSlider from '@/components/main_product_slider';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
+import MainBestProductSection from '@/components/main_best_product_section';
+import ShoppingBannerSlider from '@/components/Shopping_banner_slider';
+import MainBottomSlider from '@/components/main_bottom_slider';
 export default function Home() {
   return (
-   <>
-    <Header />
-    <main>
-      <h1>배너</h1>
-      <MainBannerSlider />
-      <h1>나혼산 BEST 상품</h1>
-      <MainProductSlider />
-    </main>
-    <Footer />
-   </>   
+    <>
+      <Header />
+      <main>
+        <MainBannerSlider />
+        <MainBestProductSection />
+
+        <ShoppingBannerSlider />
+        <MainBottomSlider />
+      </main>
+      <Footer />
+    </>
   );
 }
