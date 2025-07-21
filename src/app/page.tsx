5520559db8a2848a54c7f4deda8451419f3d9638
Yujiin <yjjeong0528@gmail.com>
2025-07-21 12:29:26 +0900

    🎨 feat: 쇼핑 서브카테고리 수정 및 메뉴 연동 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import MainBannerSlider from '@/components/main_banner_slider';
-
 import MainBestProductSection from '@/components/main_best_product_section';
 import ShoppingBannerSlider from '@/components/Shopping_banner_slider';
 import MainBottomSlider from '@/components/main_bottom_slider';
-import Header from '@/components/_common/Header';
-import Footer from '@/components/_common/Footer';
 export default function Home() {
   return (
     <>
-      <Header />
       <main>
         <MainBannerSlider />
         <MainBestProductSection />
@@ -16,7 +12,6 @@ export default function Home() {
         <ShoppingBannerSlider />
         <MainBottomSlider />
       </main>
-      <Footer />
     </>
   );
 }
--- src/app/shopping/category/page.tsx
+++ src/app/shopping/category/page.tsx
@@ -1,5 +1,3 @@
-import Footer from '@/components/common/Footer';
-import Header from '@/components/common/Header';
 import Pagenation from '@/components/Pagenation';
 import ShoppingBannerSlider from '@/components/Shopping_banner_slider';
 import ShoppingSubcategory from '@/components/Shopping_Subcategory';
@@ -7,13 +5,11 @@ import ShoppingSubcategory from '@/components/Shopping_Subcategory';
 export default function ShoppingCategory() {
   return (
     <>
-      <Header />
       <main>
         <ShoppingBannerSlider />
         <ShoppingSubcategory />
         <Pagenation />
       </main>
-      <Footer />
     </>
   );
 }
