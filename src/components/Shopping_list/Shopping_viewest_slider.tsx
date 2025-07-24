49a1bc4e4e7efc6ff69a4c2205a96b2cae78ff20
Yujiin <yjjeong0528@gmail.com>
2025-07-24 09:22:38 +0900

    🐛 fix: 서버 주소 관련 오류 수정 '/'

--- src/components/Shopping_list/Shopping_viewest_slider.tsx
+++ src/components/Shopping_list/Shopping_viewest_slider.tsx
@@ -64,6 +64,7 @@ function ShoppingViewestSlider() {
             return (
               <SwiperSlide key={product._id}>
                 <ProductCard
+                  id={product._id}
                   name={product.name}
                   imageUrl={`${API_URL}/${product.mainImages[0]?.path}`}
                   price={`${product.price.toLocaleString()}원`}
