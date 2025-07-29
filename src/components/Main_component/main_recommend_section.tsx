6049f261102c35e3bc5820a87cdd729b73731a57
Yujiin <yjjeong0528@gmail.com>
2025-07-30 00:32:28 +0900

    ♻️ refactor:  메인에 완성 기능 연결을 위한 컴포넌트 정리

---
+++ src/components/Main_component/main_recommend_section.tsx
@@ -0,0 +1,10 @@
+import Title from '@/components/Title';
+
+function MainRecommendProductSection() {
+  return (
+    <>
+      <Title title="FOR YOU" subTitle="좋아하실 만한 상품을 추천해드려요 🧐" />
+    </>
+  );
+}
+export default MainRecommendProductSection;
