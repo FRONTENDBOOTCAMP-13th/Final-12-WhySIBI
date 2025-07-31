d49a63093df42d95a047d8db6189ffbb61e63e06
yeono <126737362+KYDDD@users.noreply.github.com>
2025-07-31 10:44:01 +0900

    🎨 feat: 장바구니 선택삭제 zustand사용하여 리팩토링

--- src/app/cart/Cart_all_delete_button.tsx
+++ src/app/cart/Cart_all_delete_button.tsx
@@ -3,7 +3,7 @@
 import { deleteAllCartAction } from '@/data/actions/delete_all_cart_action';
 import useCartRefreshStore from '@/zustand/useCartRefreshStore';
 import useUserStore from '@/zustand/useUserStore';
-// import { useActionState, useEffect } from 'react';
+import { useActionState, useEffect } from 'react';
 
 export default function CartAllDeleteButton({
   checkedItems,
@@ -15,57 +15,40 @@ export default function CartAllDeleteButton({
 
   const { triggerRefresh } = useCartRefreshStore();
   // 서버액션
-  // const initialState: { status?: boolean; error: string } = {
-  //   // status: false,
-  //   error: '',
-  // };
+  const initialState: { status?: boolean; error: string } = {
+    // status: false,
+    error: '',
+  };
 
-  // const [state, formAction, isPending] = useActionState(
-  //   deleteAllCartAction,
-  //   initialState,
-  // );
+  const [state, formAction, isPending] = useActionState(
+    deleteAllCartAction,
+    initialState,
+  );
 
-  // useEffect(() => {
-  //   if (state && state.status === false) {
-  //     alert(state.error);
-  //   } else if (state && state.status === true) {
-  //     alert('성공적으로 삭제되었습니다!');
-  //   }
-  // }, [state]);
-
-  //인풋 히든으로 숫자배열 넘기면 타입에러 나서 스트링 배열로 바꿔줌...빡치네
-  // const stringItems = checkedItems.map(item => item.toString());
-  async function handleDelete() {
-    if (checkedItems.length === 0) {
-      alert('삭제할 아이템을 선택해주세요');
-    } else {
-      await deleteAllCartAction(checkedItems, token);
+  useEffect(() => {
+    if (state && state.status === false) {
+      alert(state.error);
+    } else if (state && state.status === true) {
       triggerRefresh();
     }
-  }
-  return (
-    // <form action={formAction}>
-    //   <input name="token" value={token} hidden readOnly />
-    //   <input
-    //     name="checkedItems"
-    //     value={JSON.stringify(checkedItems)}
-    //     // value={stringItems}
-    //     hidden
-    //     readOnly
-    //   />
-    //   <button
-    //     className="border-2 rounded-3xl text-button-color w-24 h-9 font-bold"
-    //     disabled={isPending}
-    //   >
-    //     선택삭제
-    //   </button>
-    // </form>
+  }, [state, triggerRefresh]);
 
-    <button
-      onClick={handleDelete}
-      className="border-2 rounded-3xl text-button-color w-24 h-9 font-bold"
-    >
-      선택삭제
-    </button>
+  return (
+    <form action={formAction}>
+      <input name="token" value={token} hidden readOnly />
+      <input
+        name="checkedItems"
+        value={JSON.stringify(checkedItems)}
+        // value={stringItems}
+        hidden
+        readOnly
+      />
+      <button
+        className="border-2 rounded-3xl text-button-color w-24 h-9 font-bold"
+        disabled={isPending}
+      >
+        선택삭제
+      </button>
+    </form>
   );
 }
--- src/data/actions/delete_all_cart_action.ts
+++ src/data/actions/delete_all_cart_action.ts
@@ -1,28 +1,27 @@
 'use server';
 
 export async function deleteAllCartAction(
-  checkedItems: number[],
-  token: string | undefined,
+  _: { status?: boolean; error: string },
+  formData: FormData,
 ) {
-  // const stringItems = formData.get('checkedItems'); //배열 데이터를 넘겨받을때는 getAll을 사용
-  // const stringItems = formData.get('checkedItems'); //배열 데이터를 넘겨받을때는 getAll을 사용
-  // const token = formData.get('token')?.toString();
+  const stringItems = formData.get('checkedItems'); //배열 데이터를 넘겨받을때는 getAll을 사용
+  const token = formData.get('token')?.toString();
 
-  // const checkedItems: number[] = stringItems?.map(item => Number(item));
-  // console.log('ㅎㅇㅎㅇ4', checkedItems);
-  // if (!checkedItems) {
-  console.log('체크 아이템즈', checkedItems);
-  if (!checkedItems) {
-    // return {
-    //   status: false,
-    //   error: '삭제할 상품이 없습니다.',
-    // };
+  console.log('ㅎㅇㅎㅇ3', stringItems);
+
+  //조건식 이렇게 하면 parse에 타입에러 없어짐
+  if (!stringItems || typeof stringItems !== 'string') {
+    return {
+      status: false,
+      error: '삭제할 상품이 없습니다.',
+    };
   }
+  const deleteList = JSON.parse(stringItems);
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/carts/`, {
       method: 'DELETE',
       body: JSON.stringify({
-        carts: checkedItems,
+        carts: deleteList,
       }),
       headers: {
         Authorization: `Bearer ${token}`,
@@ -33,16 +32,15 @@ export async function deleteAllCartAction(
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-    //재검증
-    // return {
-    //   status: true,
-    //   error: '',
-    // };
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (err) {
-    // return {
-    //   status: false,
-    //   error: `상품 삭제에 실패했습니다: ${err}`,
-    // };
-    console.error(err);
+    return {
+      status: false,
+      error: `상품 삭제에 실패했습니다: ${err}`,
+    };
   }
 }
