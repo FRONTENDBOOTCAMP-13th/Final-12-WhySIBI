707f01889d743a1eafe163df96a8c63be3e755e6
Yujiin <yjjeong0528@gmail.com>
2025-08-03 19:19:11 +0900

    🎨 feat: 검색, 인기검색어, 추천검색어 기능 완성

--- src/zustand/productSearchStore.tsx
+++ src/zustand/productSearchStore.tsx
@@ -4,10 +4,13 @@ interface ProductSearchState {
   searchText: string;
   handleSearchClick: (Text: string) => void;
   resetSearch: () => void;
+  isOpen?: boolean;
+  toggleModal: (open: boolean) => void;
 }
 
 const useProductSearchStore = create<ProductSearchState>(set => ({
   searchText: '',
+  isOpen: false,
   handleSearchClick: (Text: string) => {
     console.log(Text);
     set({
@@ -19,6 +22,7 @@ const useProductSearchStore = create<ProductSearchState>(set => ({
       searchText: '',
     });
   },
+  toggleModal: open => set({ isOpen: open }),
 }));
 
 export default useProductSearchStore;
