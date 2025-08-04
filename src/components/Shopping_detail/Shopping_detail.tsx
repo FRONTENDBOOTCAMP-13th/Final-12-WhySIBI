1327436cd7ef107519e4c849474c66ef3369cf2e
yeono <126737362+KYDDD@users.noreply.github.com>
2025-08-04 10:31:41 +0900

    🐛 fix: 이미지 경로 오류 수정

--- src/components/Shopping_detail/Shopping_detail.tsx
+++ src/components/Shopping_detail/Shopping_detail.tsx
@@ -28,7 +28,7 @@ export default async function ShoppingDetail({
       {/* 상품 사진 영역 */}
       <figure className="bg-white min-w-[600px] min-h-[600px] overflow-hidden flex justify-center items-center rounded-sm shadow-md">
         <Image
-          src={item.mainImages[0].path}
+          src={`/${item.mainImages[0].path}`}
           width={590}
           height={590}
           className="object-cover"
